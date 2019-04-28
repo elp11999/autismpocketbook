6c7b6a238865ba86186a089b528ad124dd50a252
Michael Henderson <mhenderson557@yahoo.com>
2019-04-28 12:33:42 -0500

    Initial push.

---
+++ src/components/Home/index.js
@@ -0,0 +1,24 @@
+//
+// NavBar UI Component
+//
+// index.js
+//
+
+// Import the React library
+import React from "react";
+//import './styles.css';
+
+// Import the CSS for the NavBar component
+
+// Function to construct the NavBar component of the UI
+function Home(props) {
+    //console.log(props);
+    return (
+        <div>
+            <img src="https://www.autismspeaks.org/sites/default/files/styles/hero_tablet/public/Leonardo_hero_2.jpg?h=239e4b0a&itok=t2whwMJQ" alt="pic"></img>
+        </div>
+    );
+}
+
+// Export the NavBar UI component
+export default Home;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,51 @@
+//
+// NavBar UI Component
+//
+// index.js
+//
+
+// Import the React library
+import React from "react";
+import './styles.css';
+
+// Import the CSS for the NavBar component
+
+// Function to construct the NavBar component of the UI
+function NavBar(props) {
+    //console.log(props);
+    return (
+        <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+            <a className="navbar-brand" href="/">
+            <img id="nav-bar-image" src="/autism.jpg" alt="Logo"></img></a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarText">
+                <ul>
+                    <li className="nav-item active">
+                        <a className="nav-link nav-bar-text" href="/home">
+                            <span className="navbar-bar-text">Autism Pocket Book</span>
+                        </a>
+                    </li>
+                </ul>
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/usage">How it works</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/reports">Reports</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/resources">Resources</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/login">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+// Export the NavBar UI component
+export default NavBar;
